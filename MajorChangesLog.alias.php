9b47718dd16c689933d1aaf5dc59772b3bdc868d
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2016-03-01 00:33:21 +0200

    Update special page aliases

--- MajorChangesLog.alias.php
+++ MajorChangesLog.alias.php
@@ -4,16 +4,16 @@ $specialPageAliases = array();
 
 /** English (English) */
 $specialPageAliases['en'] = array(
-	'MajorChangesLog' => array( 'Major_Changes_log' ),
+	'MajorChangesLog' => array( 'MajorChangesLog', 'Major Changes log', "Major Changes Log" ),
 );
 
 /** Hebrew (עברית) */
 $specialPageAliases['he'] = array(
-	'MajorChangesLog' => array( 'יומן_שינויים_מהותיים' ),
+	'MajorChangesLog' => array( 'יומן שינויים מהותיים', 'שינויים מהותיים' ),
 );
 
 /** Arabic (العربية) */
 $specialPageAliases['ar'] = array(
-	//'TemplateSandbox' => array( 'ملعب_قالب' ),
+	//'MajorChangesLog' => array( '' ),
 );
 
