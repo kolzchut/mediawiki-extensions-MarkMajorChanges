8810f9750e1367f9a9aa1e10121cba864991f7df
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2017-08-17 10:35:13 +0300

    Minor: remove the mediawiki global from the js code, not used

--- mediawiki.action.majorchange.js
+++ mediawiki.action.majorchange.js
@@ -1,11 +1,11 @@
 /*!
  * Scripts for action=markmajorchange at domready
  */
-( function ( mw, $ ) {
+( function ( $ ) {
 	'use strict';
 
 	$( function () {
 		// Make sure reason text does not exceed byte limit
 		$( '#mw-input-wpreason' ).byteLimit( 255 );
 	} );
-}( mediaWiki, jQuery ) );
+}( jQuery ) );
